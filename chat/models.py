9943bd9dfa08917440ad02f5b1f09e1310a60755
kaua <kauayt120@gmail.com>
2025-12-19 18:59:39 -0300

    sistema de cadastro basico feito

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from competicao.models import Sala
 
-# Create your models here.
+class Mensagem(models.Model) :
+    usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+    sala = models.ForeignKey(Sala, on_delete=models.CASCADE, related_name='mensagens')
+    texto = models.TextField(max_length=500, null=False, blank=False)
+    data_envio = models.DateTimeField(auto_now_add=True)
+    
+    class Meta :
+        ordering = ['data_envio']
+    
+    def __str__(self):
+        return f'{self.usuario.username}: {self.texto[:20]}'
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
-# Create your views here.
+
+def cadastro(request) :
+    if request.method == 'POST' :
+        form = UserCreationForm(request.POST)
+        
+        if form.is_valid() :
+            form.save()
+            messages.success(request, f'Conta criada com sucesso! Faça login para continuar.')
+            return redirect('login')
+    else :
+        form = UserCreationForm()
+            
+    return render(request, 'usuarios/cadastro.html', {'form': form})
